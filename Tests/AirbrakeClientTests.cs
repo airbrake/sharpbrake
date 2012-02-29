f79e38fb00f4ccaba6626ab919362eab1eccc135
asbjornu <asbjorn@ulsberg.no>
2012-02-29 08:33:12 +0100

    Moved all references to NuGet and removed the local 'refs' directory.

--- Tests/AirbrakeClientTests.cs
+++ Tests/AirbrakeClientTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading;
 
-
 using NUnit.Framework;
 
 using SharpBrake;
@@ -9,67 +8,67 @@ using SharpBrake.Serialization;
 
 namespace Tests
 {
-   [TestFixture]
-   public class AirbrakeClientTests
-   {
-      #region Setup/Teardown
+    [TestFixture]
+    public class AirbrakeClientTests
+    {
+        #region Setup/Teardown
 
-      [SetUp]
-      public void SetUp()
-      {
-         this.client = new AirbrakeClient();
-      }
+        [SetUp]
+        public void SetUp()
+        {
+            this.client = new AirbrakeClient();
+        }
 
-      #endregion
+        #endregion
 
-      private AirbrakeClient client;
+        private AirbrakeClient client;
 
 
-      [Test]
-      public void Send_EndRequestEventIsInvoked_And_ResponseOnlyContainsApiError()
-      {
-         bool requestEndInvoked = false;
-         AirbrakeResponseError[] errors = null;
-         int i = 0;
+        [Test]
+        public void Send_EndRequestEventIsInvoked_And_ResponseOnlyContainsApiError()
+        {
+            bool requestEndInvoked = false;
+            AirbrakeResponseError[] errors = null;
+            int i = 0;
 
-         this.client.RequestEnd += (sender, e) =>
-         {
-            requestEndInvoked = true;
-            errors = e.Response.Errors;
-         };
-
-         var configuration = new AirbrakeConfiguration
-         {
-            ApiKey = Guid.NewGuid().ToString("N"),
-            EnvironmentName = "test",
-         };
-
-         var builder = new AirbrakeNoticeBuilder(configuration);
-
-         AirbrakeNotice notice = builder.Notice(new Exception("Test"));
-
-         notice.Request = new AirbrakeRequest("http://example.com", "Test")
-         {
-            Params = new[]
+            this.client.RequestEnd += (sender, e) =>
             {
-               new AirbrakeVar("TestKey", "TestValue")
+                requestEndInvoked = true;
+                errors = e.Response.Errors;
+            };
+
+            var configuration = new AirbrakeConfiguration
+            {
+                ApiKey = Guid.NewGuid().ToString("N"),
+                EnvironmentName = "test",
+            };
+
+            var builder = new AirbrakeNoticeBuilder(configuration);
+
+            AirbrakeNotice notice = builder.Notice(new Exception("Test"));
+
+            notice.Request = new AirbrakeRequest("http://example.com", "Test")
+            {
+                Params = new[]
+                {
+                    new AirbrakeVar("TestKey", "TestValue")
+                }
+            };
+
+            this.client.Send(notice);
+
+            while (!requestEndInvoked)
+            {
+                // Sleep for maximum 5 seconds to wait for the request to end. Can probably be done more elegantly.
+                if (i++ == 50)
+                    break;
+
+                Thread.Sleep(100);
             }
-         };
 
-         this.client.Send(notice);
-
-         while (!requestEndInvoked)
-         {
-            // Sleep for maximum 5 seconds to wait for the request to end. Can probably be done more elegantly.
-            if (i++ == 50)
-               break;
-
-            Thread.Sleep(100);
-         }
-
-         Assert.That(requestEndInvoked, Is.True);
-         Assert.That(errors, Is.Not.Null);
-         Assert.That(errors, Has.Length.EqualTo(1));
-      }
-   }
+            Assert.That(requestEndInvoked, Is.True);
+            Assert.That(errors, Is.Not.Null);
+            Assert.That(errors, Has.Length.EqualTo(1));
+        }
+    }
 }
