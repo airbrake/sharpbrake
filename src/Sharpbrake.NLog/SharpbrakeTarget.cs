bcf9c749f941e605ff70370e4b57081aebfd8b98
Andriy Holovko <andriy.holovko@gmail.com>
2017-07-06 20:36:13 +0300

    Add skeleton for Sharpbrake NLog integration

---
+++ src/Sharpbrake.NLog/SharpbrakeTarget.cs
@@ -0,0 +1,35 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using Sharpbrake.Client;
+
+namespace Sharpbrake.NLog
+{
+    [Target("Sharpbrake")]
+    public sealed class SharpbrakeTarget : TargetWithLayout
+    {
+        private readonly AirbrakeNotifier notifier;
+
+        public SharpbrakeTarget()
+        {
+            var config = new AirbrakeConfig
+            {
+                ProjectId = ProjectId,
+                ProjectKey = ProjectKey
+            };
+
+            notifier = new AirbrakeNotifier(config);
+        }
+
+        [RequiredParameter]
+        public string ProjectId { get; set; }
+
+        [RequiredParameter]
+        public string ProjectKey { get; set; }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            notifier.Notify(logEvent.Exception);
+        }
+    }
+}
